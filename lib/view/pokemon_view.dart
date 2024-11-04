ad4baf115146ea1c1740ecd11aeac965b563086f
rojanparajuli <rojanparajuli13@gmail.com>
2024-11-04 18:55:48 +0545

    chnages

--- lib/view/pokemon_view.dart
+++ lib/view/pokemon_view.dart
@@ -1,5 +1,8 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:pokemon/bloc/pokemon_bloc.dart';
 import 'package:pokemon/bloc/pokemon_event.dart';
 import 'package:pokemon/bloc/pokemon_state.dart';
@@ -13,66 +16,164 @@ class PokemonSearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pokémon Search'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                labelText: 'Enter Pokémon Name',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                final pokemonName = _controller.text.trim();
-                if (pokemonName.isNotEmpty) {
-                  context.read<PokemonBloc>().add(SearchPokemon(pokemonName));
-                }
-              },
-              child: const Text('Search'),
-            ),
-            const SizedBox(height: 20),
-            BlocBuilder<PokemonBloc, PokemonState>(
-              builder: (context, state) {
-                if (state is PokemonLoading) {
-                  return const CircularProgressIndicator();
-                } else if (state is PokemonLoaded) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Name: ${state.pokemon.name}',
-                          style: const TextStyle(fontSize: 18)),
-                      Text('Height: ${state.pokemon.height}',
-                          style: const TextStyle(fontSize: 18)),
-                      Text('Weight: ${state.pokemon.weight}',
-                          style: const TextStyle(fontSize: 18)),
-                      Text('Abilites: ${state.pokemon.abilities}',
-                          style: const TextStyle(fontSize: 18)),
-                      Text('Type: ${state.pokemon.types}',
-                          style: const TextStyle(fontSize: 18)),
-                      Text('Moves: ${state.pokemon.moves}',
-                          style: const TextStyle(fontSize: 18)),
-                      Text('Cries: ${state.pokemon.cries}',
-                          style: const TextStyle(fontSize: 18)),
-                      Text('Past Abilites: ${state.pokemon.pastAbilities}',
-                          style: const TextStyle(fontSize: 18)),
-                    ],
-                  );
-                } else if (state is PokemonError) {
-                  return Text(state.message,
-                      style: const TextStyle(color: Colors.red));
-                } else {
-                  return const Text('Enter a Pokémon name to start');
-                }
-              },
-            ),
-          ],
+        title: Text(
+          'Pokémon Search',
+          style: GoogleFonts.pressStart2p(
+            fontSize: 20,
+            color: Colors.yellow,
+          ),
         ),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          Opacity(
+            opacity: 0.15,
+            child: Image.asset(
+              'assets/Pokemon Pokeball Sticker.jpg',
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Pokémon Name',
+                    labelStyle: GoogleFonts.pressStart2p(
+                      color: Colors.red,
+                      fontSize: 14,
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.red),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.red),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.yellow),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.red,
+                    ),
+                  ),
+                  style: GoogleFonts.pressStart2p(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    foregroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    textStyle: GoogleFonts.pressStart2p(fontSize: 14),
+                  ),
+                  onPressed: () {
+                    final pokemonName = _controller.text.trim();
+                    if (pokemonName.isNotEmpty) {
+                      context
+                          .read<PokemonBloc>()
+                          .add(SearchPokemon(pokemonName));
+                    }
+                  },
+                  child: const Text('Search Pokémon'),
+                ),
+                const SizedBox(height: 20),
+                BlocBuilder<PokemonBloc, PokemonState>(
+                  builder: (context, state) {
+                    if (state is PokemonLoading) {
+                      return Center(
+                        child: SizedBox(
+                            height: 200,
+                            width: 200,
+                            child: Lottie.asset('assets/loading.json')),
+                      );
+                    } else if (state is PokemonLoaded) {
+                      return Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Colors.yellow.withOpacity(0.9),
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(color: Colors.red, width: 2),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Name: ${state.pokemon.name}',
+                              style: GoogleFonts.pressStart2p(
+                                fontSize: 14,
+                                color: Colors.red,
+                              ),
+                            ),
+                            Text(
+                              'Height: ${state.pokemon.height}',
+                              style: GoogleFonts.pressStart2p(fontSize: 12),
+                            ),
+                            Text(
+                              'Weight: ${state.pokemon.weight}',
+                              style: GoogleFonts.pressStart2p(fontSize: 12),
+                            ),
+                            Text(
+                              'Abilities: ${state.pokemon.abilities?.join(", ")}',
+                              style: GoogleFonts.pressStart2p(fontSize: 12),
+                            ),
+                            Text(
+                              'Type: ${state.pokemon.types?.join(", ")}',
+                              style: GoogleFonts.pressStart2p(fontSize: 12),
+                            ),
+                            Text(
+                              'Moves: ${state.pokemon.moves?.join(", ")}',
+                              style: GoogleFonts.pressStart2p(fontSize: 12),
+                            ),
+                          ],
+                        ),
+                      );
+                    } else if (state is PokemonError) {
+                      return Text(
+                        state.message,
+                        style: GoogleFonts.pressStart2p(
+                          color: Colors.red,
+                          fontSize: 12,
+                        ),
+                      );
+                    } else {
+                      return Center(
+                        child: AnimatedTextKit(
+                          animatedTexts: [
+                            TypewriterAnimatedText(
+                              'Enter a Pokémon name to start',
+                              textStyle: GoogleFonts.pressStart2p(
+                                fontSize: 10,
+                                color: Colors.black,
+                              ),
+                              speed: const Duration(milliseconds: 100),
+                            ),
+                          ],
+                          repeatForever: true,
+                          pause: const Duration(milliseconds: 1000),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
